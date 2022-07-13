42b3b1fd025d12e42307d0a6c295190663b19154
unknown <22220252@KTNG.AD>
2022-07-13 14:07:58 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [items, setItems] = useState([{}]);
+  useEffect(() => {
+    axios.get("https://sshtht-springboot-mariadb.herokuapp.com/board/selectall").then((res) => {
+      setItems([...res.data]);
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {items.map((v, i) => (
+        <li key={i}>{v.todoList}</li>
+      ))}
+    </>
   );
 }
 
